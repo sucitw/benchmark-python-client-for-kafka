1cc3d24afbe89920ed6d098c71c389a2ae0f43a5
shuhsi <sucitw@gmail.com>
2017-03-19 19:25:45 +0800

    add __init__.py for kafka_client modules

--- benchmark.py
+++ benchmark.py
@@ -7,10 +7,7 @@
 """
 
 import time
-
-msg_count = 1000000
-msg_size = 100
-msg_payload = ('pycontw 2017' * 20).encode()[:msg_size]
+import kafka_client.client_pykafka as pykafka1
 
 
 producer_timings = {}
@@ -20,3 +17,7 @@ def calculate_thoughput(timing, n_messages=1000000, msg_size=100):
     print("Processed {0} messsages in {1:.2f} seconds".format(n_messages, timing))
     print("{0:.2f} MB/s".format((msg_size * n_messages) / timing / (1024*1024)))
     print("{0:.2f} Msgs/s".format(n_messages / timing))
+
+
+producer_timings['pykafka_producer'] = pykafka1.pykafka_producer_performance()
+calculate_thoughput(producer_timings['pykafka_producer'])
